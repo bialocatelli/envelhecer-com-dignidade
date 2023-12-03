b93b319abe41647fa68dadc9cd9d40446e88c87a
Beatriz Locatelli <beatrizglocatelli@gmail.com>
2023-12-03 03:33:34 -0300

    subindo classe PessoaIdosa e testes.

---
+++ Classes/PessoaIdosa/PessoaIdosa.js
@@ -0,0 +1,42 @@
+const Pessoa = require("../Pessoa/Pessoa")
+
+class PessoaIdosa extends Pessoa {
+    idade;
+    #endereco;
+    mobilidadeReduzida;
+    condicaoEspecial;
+    static listaPessoasIdososas = []
+
+    constructor(nome, email, telefone, idade, endereco) {
+        super(nome, email, telefone);
+        this.idade = idade;
+        this.#endereco = endereco;
+        this.constructor.listaPessoasIdososas.push({ nome: this.nome, telefone: this.telefone, idade: this.idade })
+    }
+
+    getEndereco() {
+        return this.#endereco
+    }
+
+    setEndereco(novoEndereco) {
+        return this.#endereco = novoEndereco
+    }
+
+    cadastrarPessoaIdosa(nome, email, telefone, idade, endereco) {
+        const idoso = idade >= 60
+
+        if (!idoso) {
+            throw `Não foi possível realizar seu cadastro. Idade mínima tem que ser de 60 anos.`
+        } else {
+            this.nome = nome;
+            this.email = email;
+            this.telefone = telefone;
+            this.idade = idade;
+            this.#endereco = endereco;
+            return `Olá, seu cadastro foi realizado com sucesso!`
+        }
+    }
+
+
+}
+module.exports = PessoaIdosa
---
+++ Classes/PessoaIdosa/PessoaIdosa.test.js
@@ -0,0 +1,37 @@
+const PessoaIdosa = require("./PessoaIdosa")
+
+describe("Teste da Classe PessoaIdosa", () => {
+    test("Verifica se objeto pessoaIdosa está sendo instanciado.", () => {
+        const pessoaIdosa = new PessoaIdosa();
+        expect(pessoaIdosa instanceof PessoaIdosa).toBe(true)
+    })
+
+    test("Instanciar objeto pessoaIdosa com valores corretos", () => {
+        const pessoaIdosa = new PessoaIdosa("Bia", "bia@email.com", "11444455588", 61, "Rua João, 241")
+        expect(pessoaIdosa.nome).toBe("Bia");
+        expect(pessoaIdosa.email).toBe("bia@email.com");
+        expect(pessoaIdosa.telefone).toBe("11444455588");
+        expect(pessoaIdosa.idade).toBe(61);
+        expect(pessoaIdosa.getEndereco()).toBe("Rua João, 241")
+    })
+
+    test("Retorna mensagem de sucesso ao cadastrar pessoa idosa.", () => {
+        const pessoaIdosa = new PessoaIdosa()
+        expect(pessoaIdosa.cadastrarPessoaIdosa("Bia", "bia@email.com", "11444455588", 61, "Rua João, 241")).toBe(
+            `Olá, seu cadastro foi realizado com sucesso!`
+        )
+        expect(pessoaIdosa.nome).toBe("Bia");
+        expect(pessoaIdosa.email).toBe("bia@email.com");
+        expect(pessoaIdosa.telefone).toBe("11444455588");
+        expect(pessoaIdosa.idade).toBe(61);
+        expect(pessoaIdosa.getEndereco()).toBe("Rua João, 241")
+    })
+
+    test("Retorna mensagem de erro ao tentar cadatrar pessoa idosa com valores icorretos", () => {
+        const pessoaIdosa = new PessoaIdosa()
+        expect(() => pessoaIdosa.cadastrarPessoaIdosa("Bia", "bia@email.com", "11444455588", 59, "Rua João, 241")).toThrow(`Não foi possível realizar seu cadastro. Idade mínima tem que ser de 60 anos.`);
+    })
+
+
+    
+})
